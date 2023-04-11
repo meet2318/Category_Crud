d7e513a478f29d06f35656f55cb0493036237231
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-04-11 16:47:31 +0530

    Add files via upload
    for development

---
+++ AdminTagsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using Web_Blogging.Data;
+using Web_Blogging.Models.Domain;
+using Web_Blogging.Models.ViewModels;
+
+namespace Web_Blogging.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag Domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ BloggieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Blogging.Models.Domain;
+
+namespace Web_Blogging.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
